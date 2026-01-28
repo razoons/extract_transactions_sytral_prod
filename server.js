9f21bae736f59de0fb7e1315d23b6a28be83fae5
antoine.sarazin <antoine.sarazin@instant-system.com>
2026-01-28 17:45:09 +0100

    Correction du message de résultat final pour les transactions avec montants différents

--- server.js
+++ server.js
@@ -373,11 +373,12 @@ function build_extract(results_headers, results_monetico, results_monetico_selec
             if (transaction.moneticoAmount == transaction.conduentAmount) {
               transaction.finalResult = "OK - Produit distribué et payé par CB";
             } else {
-              if (transaction.sumProductsAmount == transaction.moneticoAmount){
+              transaction.finalResult = "KO - Produit distribué et payé par CB mais montants différents";
+              /*if (transaction.sumProductsAmount == transaction.moneticoAmount){
                 transaction.finalResult = "KO - Produit distribué et payé par CB mais somme des paniers différente de Monetico";
               }else{
                 transaction.finalResult = "KO - Produit distribué et payé par CB mais les paniers IS et Conduent diffèrent";
-              }
+              }*/
             }
           } else if (transaction.conduentCheck == "Mauvais Statut") {
             transaction.finalResult = "KO - Produit payé par CB mais pas distribué";
